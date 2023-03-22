2e7a10e5023fb662f4abc92a52f06c69ddcee21f
esteban sustacha <esustachah@gmail.com>
2023-03-22 02:47:02 -0400

    Updated App
    Created components, modified main.js to reflect components, added code for App.vue, modified index.html to reflect interface, created pinia store as a const obj
    
    added axiom dependency to project

---
+++ src/App.vue
@@ -0,0 +1,41 @@
+<template>
+  <div id="app">
+    <api-controls />
+    <current-chat-viewport />
+    <message-history />
+    <user-message-text-box />
+  </div>
+</template>
+
+<script>
+import { useStore } from 'pinia';
+
+export default {
+  setup() {
+    const store = useStore();
+    const messages = store.state.messages;
+
+    const connect = async () => {
+      // connect to chat GPT API
+      // store the connection in the store
+    };
+
+    const disconnect = async () => {
+      // disconnect from chat GPT API
+      // remove the connection from the store
+    };
+
+    const sendMessage = async (message) => {
+      // send message to chat GPT API
+      // add the message to the store
+    };
+
+    return {
+      messages,
+      connect,
+      disconnect,
+      sendMessage
+    };
+  }
+};
+</script>
