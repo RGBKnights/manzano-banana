728a2883d5df4bf8632520267ded29bd62c789cc
RGBKnights <master@rgbknights.com>
2023-03-22 19:29:25 -0400

    reset

--- src/App.vue
+++ src/App.vue
@@ -1,41 +1,30 @@
+<script setup>
+import HelloWorld from './components/HelloWorld.vue'
+</script>
+
 <template>
-  <div id="app">
-    <api-controls />
-    <current-chat-viewport />
-    <message-history />
-    <user-message-text-box />
+  <div>
+    <a href="https://vitejs.dev" target="_blank">
+      <img src="/vite.svg" class="logo" alt="Vite logo" />
+    </a>
+    <a href="https://vuejs.org/" target="_blank">
+      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
+    </a>
   </div>
+  <HelloWorld msg="Vite + Vue" />
 </template>
 
-<script>
-import { useStore } from 'pinia';
-
-export default {
-  setup() {
-    const store = useStore();
-    const messages = store.state.messages;
-
-    const connect = async () => {
-      // connect to chat GPT API
-      // store the connection in the store
-    };
-
-    const disconnect = async () => {
-      // disconnect from chat GPT API
-      // remove the connection from the store
-    };
-
-    const sendMessage = async (message) => {
-      // send message to chat GPT API
-      // add the message to the store
-    };
-
-    return {
-      messages,
-      connect,
-      disconnect,
-      sendMessage
-    };
-  }
-};
-</script>
+<style scoped>
+.logo {
+  height: 6em;
+  padding: 1.5em;
+  will-change: filter;
+  transition: filter 300ms;
+}
+.logo:hover {
+  filter: drop-shadow(0 0 2em #646cffaa);
+}
+.logo.vue:hover {
+  filter: drop-shadow(0 0 2em #42b883aa);
+}
+</style>
--- src/main.js
+++ src/main.js
@@ -1,32 +1,5 @@
-import { createApp, defineComponent } from 'vue';
-import './style.css';
-import axios from 'axios';
-import { store } from './stores/storeTest.js';
-import { createChatAPI } from 'openai';
-import MessageHistory from './components/MessageHistory.vue';
-import ChatViewport from './components/CurrentChatViewport.vue';
-import UserMessageBox from './components/UserMessageTextBox.vue';
-import ApiControls from './components/ApiControls.vue';
+import { createApp } from 'vue'
+import './style.css'
+import App from './App.vue'
 
-const App = defineComponent({
-  components: {
-    MessageHistory,
-    ChatViewport,
-    UserMessageBox,
-    ApiControls,
-  },
-  setup() {
-    return {};
-  },
-});
-
-const app = createApp(App);
-
-app.component('MessageHistory', MessageHistory);
-app.component('ChatViewport', ChatViewport);
-app.component('UserMessageBox', UserMessageBox);
-app.component('ApiControls', ApiControls);
-
-app.use(store);
-
-app.mount('#app');
+createApp(App).mount('#app')
