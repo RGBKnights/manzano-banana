7f4bc9034102ea5c36e47b59a889975ecc30e72c
RGBKnights <master@rgbknights.com>
2023-03-24 20:33:29 -0400

    stlying

--- src/App.vue
+++ src/App.vue
@@ -5,22 +5,41 @@ import Threads from './components/Threads.vue'
 </script>
 
 <template>
-  <div class="h-screen w-screen font-sans leading-normal tracking-normal flex ">
-    <div class="h-screen w-1/6 bg-gradient-to-r from-amber-500 to-amber-300">
-      <Threads/>
+  <div class="content font-sans leading-normal tracking-normal h-screen">
+    <Threads/>
+    <div class="fixed top-0 left-0 z-10 w-full h-16 bg-white border-gray-200">
+      <h1 class="text-5xl text-center font-extrabold text-gray-900"><span class="text-transparent bg-clip-text bg-gradient-to-r to-emerald-600 from-amber-400">Manzano</span> Banana</h1>
     </div>
-    <div class="flex flex-col justify-end h-screen w-full bg-no-repeat bg-center bg-gradient-to-r from-amber-300 to-violet-800">
-      <div>
-        <div class="overflow-y-auto">
-          <ChatMessages/>
-        </div>
-        <div>
-          <ChatInput/>
-        </div>
-      </div>
+    <div class="">
+      <ChatMessages/> 
+    </div>
+    <div class="fixed bottom-0 left-0 z-10 w-full h-20 p-2 bg-transparent border-gray-200">
+      <ChatInput/>
     </div>
   </div>
 </template>
 
-<style scoped></style>
+<style scoped>
+@import url('https://fonts.googleapis.com/css?family=Lobster');
+h1 {
+  font-family: 'Lobster', sans-serif;
+}
+
+.content::before {
+  content: "";
+  position: fixed;
+  top: 0;
+  left: 0;
+  z-index: -1;
+  
+  display: block;
+  background-image: url('https://i.imgur.com/ymaQMmZ.png');
+  background-repeat: no-repeat;
+  background-position: center;
+  width: 100%;
+  height: 100%;
+
+  filter: blur(4px);
+}
+</style>
 
