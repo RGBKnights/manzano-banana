f63a88cb64cf1ebb25115f208eb14b1784d10c53
RGBKnights <master@rgbknights.com>
2023-03-31 01:43:13 -0400

    Stuff and Things

--- src/components/ChatInput.vue
+++ src/components/ChatInput.vue
@@ -1,7 +1,8 @@
 <template>
-  <div class="h-full container mx-auto pt-5 flex">
+  <div class="h-full container mx-auto flex">
     <div class="grow">
-        <textarea @keydown.enter.exact.prevent="sendMessage"  v-model="message" v-bind:disabled="isBusy" role="5" type="input" id="chat" class="block bg-white w-full border border-slate-300 rounded-md h-24 py-2 pr-3 pl-2 pr-9 shadow-sm focus:outline-none focus:border-sky-500 focus:ring-sky-500 focus:ring-1 sm:text-sm"></textarea>
+      <MdEditor v-model="message" language="en-US" :preview="false" :footers="[]"  :toolbarsExclude="['github', 'catalog', 'htmlPreview', 'fullscreen']" />
+      <!--<textarea @keydown.enter.exact.prevent="sendMessage"  v-model="message" v-bind:disabled="isBusy" role="5" type="input" id="chat" class="block bg-white w-full border border-slate-300 rounded-md h-24 py-2 pr-3 pl-2 pr-9 shadow-sm focus:outline-none focus:border-sky-500 focus:ring-sky-500 focus:ring-1 sm:text-sm"></textarea>-->
     </div>
     <div class="flex-none pl-5 pt-8">
       <button type="button" @click="sendMessage"  v-bind:disabled="isBusy" class="text-yellow-400 text-3xl">
@@ -16,7 +17,9 @@
   import { defineComponent, ref  } from 'vue';
   import { mapState } from 'pinia'
   import { messageStore } from '../stores/messageStore.js';
-  
+  import MdEditor from "md-editor-v3";
+  import "md-editor-v3/lib/style.css";
+
   export default defineComponent({
     name: 'ChatInput',
     setup() {
@@ -31,6 +34,9 @@
   
       return { message, sendMessage };
     },
+    components: {
+      MdEditor,
+    },
     computed: {
       ...mapState(messageStore, ['isBusy']),
       ...mapState(messageStore, ['isNotEmpty']),
@@ -39,6 +45,8 @@
   </script>
   
   <style scoped>
-
+  .md-editor {
+    height: 130px;
+  }
   </style>
   
---
+++ src/components/ChatMessage.vue
@@ -0,0 +1,30 @@
+<template>
+ <div class="p-4 mt-2 bg-white border border-gray-200 rounded-lg shadow">
+  <div class="flex space-x-4">
+    <div class="flex-shrink-0 w-8" @click="onChange">
+      <img v-if="message.role == 'system'" class="w-8 h-8 rounded-md border" src="https://i.imgur.com/eiMpBqJ.png" alt="Ai" />
+      <img v-if="message.role == 'assistant'" class="w-8 h-8 rounded-md border" src="https://i.imgur.com/eiMpBqJ.png" alt="Ai" />
+      <img v-if="message.role == 'user'" class="w-8 h-8 rounded-md border" src="https://via.placeholder.com/100x100/ffffff/000000?text=Me" alt="Me" />
+    </div>
+    <div class="flex-1 min-w-0">
+      <MdEditor v-if="preview" v-model="message.content" :previewOnly="true" :autoDetectCode="true" language="en-US"/>
+      <MdEditor v-if="!preview" v-model="message.content" language="en-US" :autoDetectCode="true" :toolbarsExclude="['github', 'catalog', 'htmlPreview', 'fullscreen']" />
+    </div>
+  </div>
+ </div>
+</template>
+
+<script setup>
+import { ref } from "vue";
+import MdEditor from "md-editor-v3";
+import "md-editor-v3/lib/style.css";
+
+const props = defineProps(['message'])
+const preview = ref(true);
+function onChange() {
+  preview.value = !preview.value;
+}
+</script>
+
+<style>
+</style>
