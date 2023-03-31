05fc8d5a9611c1345b361e97d853b947341b1537
RGBKnights <master@rgbknights.com>
2023-03-30 22:21:49 -0400

    Added a better MD handler

--- src/components/ChatInput.vue
+++ src/components/ChatInput.vue
@@ -1,12 +1,14 @@
 <template>
-  <div class="h-full w-full">
-    <label class="relative block">
-      <span class="sr-only">Chat</span>
-      <button type="button" @click="sendMessage"  v-bind:disabled="isBusy" class="absolute inset-y-0 right-0 flex items-center pr-2 text-yellow-400">
+  <div class="h-full w-full pt-5 flex">
+    <div class="grow">
+        <textarea @keydown.enter.exact.prevent="sendMessage"  v-model="message" v-bind:disabled="isBusy" role="5" type="input" id="chat" class="block bg-white w-full border border-slate-300 rounded-md h-24 py-2 pr-3 pl-2 pr-9 shadow-sm focus:outline-none focus:border-sky-500 focus:ring-sky-500 focus:ring-1 sm:text-sm"></textarea>
+    </div>
+    <div class="flex-none pl-5 pt-8">
+      <button type="button" @click="sendMessage"  v-bind:disabled="isBusy" class="text-yellow-400 text-3xl">
         <font-awesome-icon icon="fa-paper-plane" />
       </button>
-      <input @keyup.enter="sendMessage" v-model="message" v-bind:disabled="isBusy" type="input" id="chat" class="block bg-white w-full border border-slate-300 rounded-md py-2 pr-3 pl-2 pr-9 shadow-sm focus:outline-none focus:border-sky-500 focus:ring-sky-500 focus:ring-1 sm:text-sm">
-    </label>
+    </div>
+    
   </div>
 </template>
 
--- src/components/ChatMessages.vue
+++ src/components/ChatMessages.vue
@@ -1,101 +1,168 @@
 <template>
-  <template v-for="(message) of store.messages">
-    <div class="p-4 mt-2 bg-white border border-gray-200 rounded-lg shadow">
-      <div class="flex items-center space-x-4">
+  <div>
+    <template v-for="message of store.messages">
+      <div class="p-4 mt-2 bg-white border border-gray-200 rounded-lg shadow">
+        <div class="flex items-center space-x-4">
           <div class="flex-shrink-0">
-            <img v-if="message.role == 'system'" class="w-8 h-8 rounded-md border" src="https://i.imgur.com/eiMpBqJ.png" alt="Ai">
-            <img v-if="message.role == 'assistant'" class="w-8 h-8 rounded-md border" src="https://i.imgur.com/eiMpBqJ.png" alt="Ai">
-            <img v-if="message.role == 'user'" class="w-8 h-8 rounded-md border" src="https://via.placeholder.com/100x100/ffffff/000000?text=Me" alt="Me">
+            <img
+              v-if="message.role == 'system'"
+              class="w-8 h-8 rounded-md border"
+              src="https://i.imgur.com/eiMpBqJ.png"
+              alt="Ai"
+            />
+            <img
+              v-if="message.role == 'assistant'"
+              class="w-8 h-8 rounded-md border"
+              src="https://i.imgur.com/eiMpBqJ.png"
+              alt="Ai"
+            />
+            <img
+              v-if="message.role == 'user'"
+              class="w-8 h-8 rounded-md border"
+              src="https://via.placeholder.com/100x100/ffffff/000000?text=Me"
+              alt="Me"
+            />
           </div>
           <div class="flex-1 min-w-0">
             <p class="text-sm text-gray-500 break-word">
-              <vue-markdown :source="message.content" :options="markdownOptions" ></vue-markdown>
+              <MdEditor v-model="message.content" previewOnly />
             </p>
           </div>
         </div>
-    </div>
-  </template>
-  <div v-if="!isBusy" class="p-4 mt-2 bg-white border border-gray-200 rounded-lg shadow">
-    <div class="flex items-center space-x-4">
+      </div>
+    </template>
+    <div v-if="isBusy" class="p-4 mt-2 animate-pulse bg-white border border-gray-200 rounded-lg shadow">
+      <div class="flex items-center space-x-4">
         <div class="flex-shrink-0">
-          <img class="w-8 h-8 rounded-md border" src="https://i.imgur.com/eiMpBqJ.png" alt="Ai">
+          <img
+            class="w-8 h-8 rounded-md border"
+            src="https://i.imgur.com/eiMpBqJ.png"
+            alt="Ai"
+          />
         </div>
         <div class="flex-1 min-w-0">
-          <div class="text-sm text-gray-500 break-word">            
+          <div class="text-sm text-gray-500 break-word">
             <div role="status" class="space-y-2.5 max-w-lg">
-                <div class="an1sec flex items-center w-full space-x-2">
-                    <div class="h-2.5 bg-gray-200 rounded-full dark:bg-gray-700 w-32"></div>
-                    <div class="h-2.5 bg-gray-300 rounded-full dark:bg-gray-600 w-24"></div>
-                    <div class="h-2.5 bg-gray-300 rounded-full dark:bg-gray-600 w-full"></div>
-                </div>
-                <div class="an2sec flex items-center w-full space-x-2 max-w-[480px]">
-                    <div class="h-2.5 bg-gray-200 rounded-full dark:bg-gray-700 w-full"></div>
-                    <div class="h-2.5 bg-gray-300 rounded-full dark:bg-gray-600 w-full"></div>
-                    <div class="h-2.5 bg-gray-300 rounded-full dark:bg-gray-600 w-24"></div>
-                </div>
-                <div class="an3sec flex items-center w-full space-x-2 max-w-[400px]">
-                    <div class="h-2.5 bg-gray-300 rounded-full dark:bg-gray-600 w-full"></div>
-                    <div class="h-2.5 bg-gray-200 rounded-full dark:bg-gray-700 w-80"></div>
-                    <div class="h-2.5 bg-gray-300 rounded-full dark:bg-gray-600 w-full"></div>
-                </div>
-                <div class="an4sec flex items-center w-full space-x-2 max-w-[480px]">
-                    <div class="h-2.5 bg-gray-200 rounded-full dark:bg-gray-700 w-full"></div>
-                    <div class="h-2.5 bg-gray-300 rounded-full dark:bg-gray-600 w-full"></div>
-                    <div class="h-2.5 bg-gray-300 rounded-full dark:bg-gray-600 w-24"></div>
-                </div>
-                <div class="an5sec flex items-center w-full space-x-2 max-w-[440px]">
-                    <div class="h-2.5 bg-gray-300 rounded-full dark:bg-gray-600 w-32"></div>
-                    <div class="h-2.5 bg-gray-300 rounded-full dark:bg-gray-600 w-24"></div>
-                    <div class="h-2.5 bg-gray-200 rounded-full dark:bg-gray-700 w-full"></div>
-                </div>
-                <div class="an6sec flex items-center w-full space-x-2 max-w-[360px]">
-                    <div class="h-2.5 bg-gray-300 rounded-full dark:bg-gray-600 w-full"></div>
-                    <div class="h-2.5 bg-gray-200 rounded-full dark:bg-gray-700 w-80"></div>
-                    <div class="h-2.5 bg-gray-300 rounded-full dark:bg-gray-600 w-full"></div>
-                </div>
-                <span class="sr-only">Loading...</span>
+              <div class="an1 flex items-center w-full space-x-2">
+                <div
+                  class="h-2.5 bg-gray-200 rounded-full dark:bg-gray-700 w-32"
+                ></div>
+                <div
+                  class="h-2.5 bg-gray-300 rounded-full dark:bg-gray-600 w-24"
+                ></div>
+                <div
+                  class="h-2.5 bg-gray-300 rounded-full dark:bg-gray-600 w-full"
+                ></div>
+              </div>
+              <div class="an2 flex items-center w-full space-x-2 max-w-[480px]">
+                <div
+                  class="h-2.5 bg-gray-200 rounded-full dark:bg-gray-700 w-full"
+                ></div>
+                <div
+                  class="h-2.5 bg-gray-300 rounded-full dark:bg-gray-600 w-full"
+                ></div>
+                <div
+                  class="h-2.5 bg-gray-300 rounded-full dark:bg-gray-600 w-24"
+                ></div>
+              </div>
+              <div class="an3 flex items-center w-full space-x-2 max-w-[400px]">
+                <div
+                  class="h-2.5 bg-gray-300 rounded-full dark:bg-gray-600 w-full"
+                ></div>
+                <div
+                  class="h-2.5 bg-gray-200 rounded-full dark:bg-gray-700 w-80"
+                ></div>
+                <div
+                  class="h-2.5 bg-gray-300 rounded-full dark:bg-gray-600 w-full"
+                ></div>
+              </div>
+              <div class="an4 flex items-center w-full space-x-2 max-w-[480px]">
+                <div
+                  class="h-2.5 bg-gray-200 rounded-full dark:bg-gray-700 w-full"
+                ></div>
+                <div
+                  class="h-2.5 bg-gray-300 rounded-full dark:bg-gray-600 w-full"
+                ></div>
+                <div
+                  class="h-2.5 bg-gray-300 rounded-full dark:bg-gray-600 w-24"
+                ></div>
+              </div>
+              <div class="an5 flex items-center w-full space-x-2 max-w-[440px]">
+                <div
+                  class="h-2.5 bg-gray-300 rounded-full dark:bg-gray-600 w-32"
+                ></div>
+                <div
+                  class="h-2.5 bg-gray-300 rounded-full dark:bg-gray-600 w-24"
+                ></div>
+                <div
+                  class="h-2.5 bg-gray-200 rounded-full dark:bg-gray-700 w-full"
+                ></div>
+              </div>
+              <div class="an6 flex items-center w-full space-x-2 max-w-[360px]">
+                <div
+                  class="h-2.5 bg-gray-300 rounded-full dark:bg-gray-600 w-full"
+                ></div>
+                <div
+                  class="h-2.5 bg-gray-200 rounded-full dark:bg-gray-700 w-80"
+                ></div>
+                <div
+                  class="h-2.5 bg-gray-300 rounded-full dark:bg-gray-600 w-full"
+                ></div>
+              </div>
+              <span class="sr-only">Loading...</span>
             </div>
           </div>
         </div>
       </div>
+    </div>
   </div>
 </template>
 
 <script>
-import { defineComponent } from 'vue';
-import { messageStore } from '../stores/messageStore.js';
-import { mapState } from 'pinia'
-import VueMarkdown from 'vue-markdown-render'
+import { defineComponent } from "vue";
+import { messageStore } from "../stores/messageStore.js";
+import { mapState } from "pinia";
+import MdEditor from "md-editor-v3";
+import "md-editor-v3/lib/style.css";
 
 export default defineComponent({
-  name: 'ChatMessages',
+  name: "ChatMessages",
   setup() {
-      const markdownOptions = {
-        breaks: true,
-        linkify: true,
-        typographer:  true,
-      };
-      const store = messageStore()
-      const changeThread = () => {
-          store.changeThread(i);
-      };
-      return { store, changeThread, markdownOptions };
+    const store = messageStore();
+    const changeThread = () => {
+      store.changeThread(i);
+    };
+    return { store, changeThread };
+  },
+  updated() {
+    this.$nextTick(() => this.scrollToEnd());
   },
   computed: {
-    ...mapState(messageStore, ['isBusy'])
+    ...mapState(messageStore, ["isBusy"]),
   },
   components: {
-    VueMarkdown
-  }
+    MdEditor,
+  },
+  methods: {
+    scrollToEnd: function () {
+      // window.scrollTo(
+      //   {
+      //     top: document.body.scrollHeight,
+      //     left: 0,
+      //     behavior: "smooth"
+      //   }
+      // );
+    },
+  },
 });
 </script>
 
 <style lang="postcss">
 a {
-  @apply font-medium text-blue-600 dark:text-blue-500 hover:underline
+  @apply font-medium text-blue-600 dark:text-blue-500 hover:underline;
 }
 p {
-  @apply pb-1
+  @apply pb-1;
 }
 
 @keyframes example {
@@ -107,46 +174,50 @@ p {
   }
 }
 
-.an1sec {
-  opacity: 0;
-  animation-fill-mode: forwards;
-  animation-name: example;
-  animation-duration: 1s;
-}
-
-.an2sec {
-  opacity: 0;
-  animation-fill-mode: forwards;
-  animation-name: example;
-  animation-duration: 2s;
-}
-
-.an3sec {
+.an1 {
   opacity: 0;
   animation-fill-mode: forwards;
   animation-name: example;
   animation-duration: 3s;
 }
 
-.an4sec {
+.an2 {
   opacity: 0;
   animation-fill-mode: forwards;
   animation-name: example;
-  animation-duration: 4s;
+  animation-duration: 3s;
+  animation-delay: 3s;
 }
 
-.an5sec {
+.an3 {
   opacity: 0;
   animation-fill-mode: forwards;
   animation-name: example;
-  animation-duration: 5s;
+  animation-duration: 3s;
+  animation-delay: 6s;
 }
 
-.an6sec {
+.an4 {
   opacity: 0;
   animation-fill-mode: forwards;
   animation-name: example;
-  animation-duration: 6s;
+  animation-duration: 3s;
+  animation-delay: 9s;
+}
+
+.an5 {
+  opacity: 0;
+  animation-fill-mode: forwards;
+  animation-name: example;
+  animation-duration: 3s;
+  animation-delay: 12s;
+}
+
+.an6 {
+  opacity: 0;
+  animation-fill-mode: forwards;
+  animation-name: example;
+  animation-duration: 3s;
+  animation-delay: 16s;
 }
 </style>
-
